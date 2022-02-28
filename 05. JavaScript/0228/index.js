6224af35ba3fba4205cc68b7108d6b4878aa321a
홍준호 <97264086+HongJunny@users.noreply.github.com>
2022-02-28 15:05:40 +0900

    feat : JavaScript

---
+++ 05. JavaScript/0228/index.js
@@ -0,0 +1,28 @@
+//자바 print
+// System.out.println("Hello World");
+// 자바스크립트 print
+console.log('Hello World');
+var x = 10;
+var y = '안녕하세요';
+console.log('x의 값은 ? '+x);
+console.log('y의 값은 ? '+y);
+
+var num = 10;
+var num2= 20;
+var result = num+num2;
+console.log(result);
+if(result == 30) {
+    console.log('30 입니다')
+}
+
+for(var i=0; i<10; i++) {
+    console.log(i)
+}
+console.log();
+console.log();
+
+for(var i=1; i<=10; i++) {
+    if((i%2) == 0) {
+        console.log(i+' 는(은) 짝수 입니다.') 
+    }
+}
